2dff3e279c432c52e8b168469f4bde4f6762d8f2
YuliiaPaziura <50369101+YuliiaPaziura@users.noreply.github.com>
2022-10-17 18:10:13 +0300

    Fix Back to catalog link

--- src/components/pages/SingleProductPage/SingleProductPage.js
+++ src/components/pages/SingleProductPage/SingleProductPage.js
@@ -4,11 +4,10 @@ import { Link, useParams } from "react-router-dom";
 const SingleProductPage = ({rootType, rootId}) => {
 
     const {type, id} = useParams();
-    console.log(type, id)
     return (
 
         <>
-        <Link to={`/catalog/armchairs`}> Back to catalog</Link>
+        <Link to={`/catalog/${type}`}> Back to catalog</Link>
         <p>{products[type][id].name}</p>
         <p>{products[type][id].descr}</p>
         <p>{products[type][id].price}</p>
