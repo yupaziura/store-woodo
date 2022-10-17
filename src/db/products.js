90d2a3f0b931d47fa5fd2d0270dc2681ed8d663b
YuliiaPaziura <50369101+YuliiaPaziura@users.noreply.github.com>
2022-10-17 12:36:00 +0300

    Create test database

---
+++ src/db/products.js
@@ -0,0 +1,64 @@
+export const products = {
+    armchair: [
+        {
+            id: '0',
+            name: 'arm-0', 
+            descr: 'descr arm 0',
+            type: 'oak-whiteStripe',
+            img: '',
+            price: '0',
+        },
+        {
+            id: '1',
+            name: 'arm-1', 
+            descr: 'descr arm 1',
+            type: 'oak-blackStripe',
+            img: '',
+            price: '1',
+        },
+        {
+            id: '2',
+            name: 'arm-2', 
+            descr: 'descr arm 2',
+            type: 'ash-whiteStripe',
+            img: '',
+            price: '2',
+        },
+    ],
+    table: [
+        {
+            id: '0',
+            name: 'tbl-0', 
+            descr: 'descr tbl 0',
+            type: 'oak',
+            img: '',
+            price: '0',
+        },
+        {
+            id: '1',
+            name: 'tbl-1', 
+            descr: 'descr tbl 1',
+            type: 'ash',
+            img: '',
+            price: '1',
+        },
+    ],
+    accessoire: [
+        {
+            id: '0',
+            name: 'acc-0', 
+            descr: 'descr acc 0',
+            type: 'ash-large',
+            img: '',
+            price: '0',
+        },
+        {
+            id: '1',
+            name: 'acc-1', 
+            descr: 'descr acc 1',
+            type: 'ash-small',
+            img: '',
+            price: '1',
+        },
+    ],
+}
