c535deb02bde00231a2f8a0df414e42aec26e30e
YuliiaPaziura <50369101+YuliiaPaziura@users.noreply.github.com>
2022-10-18 11:47:22 +0300

    Add some changes tomain page style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,43 @@
-import {Link, NavLink} from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 const Header = () => {
     return (
         <>
-            <Link to="/">Main</Link>
-                <NavLink to="/catalog">Catalog</NavLink>
-                <NavLink to="/contacts">Contacts</NavLink>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <LinkContainer to="/"><Navbar.Brand>Main</Navbar.Brand></LinkContainer>
+                
+
+                <Nav className="me-auto">
+                    <Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <LinkContainer to="/catalog"><NavDropdown.Item>Catalog</NavDropdown.Item></LinkContainer>
+                                <NavDropdown.Divider />
+                                <LinkContainer to="/catalog/armchairs"><NavDropdown.Item >Armchairs</NavDropdown.Item></LinkContainer>
+                                <LinkContainer to="/catalog/tables"><NavDropdown.Item >Tables</NavDropdown.Item></LinkContainer>
+                                <LinkContainer to="/catalog/accessoires"><NavDropdown.Item>Accessoires</NavDropdown.Item></LinkContainer>
+                        </NavDropdown>
+                    </Nav.Link>
+
+
+
+                    <Nav.Link>
+                        <LinkContainer to="/contacts">
+                            <Nav.Item>
+                                Contacts
+                            </Nav.Item>
+                        </LinkContainer>
+                    </Nav.Link>
+                </Nav>
+
+            </Container>
+        </Navbar>
         </>
     )
 }
