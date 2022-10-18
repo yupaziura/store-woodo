d0630dc1638869944217677b1a43615bf6f60a26
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-18 20:39:01 +0300

    Add style to single product page

--- src/components/pages/SingleProductPage/SingleProductPage.js
+++ src/components/pages/SingleProductPage/SingleProductPage.js
@@ -1,18 +1,37 @@
 import { products } from "../../../db/products";
 import { Link, useParams } from "react-router-dom";
 
+import CustomButton from "../../Button/Button";
+
+import './SingleProductPage.scss';
+
 const SingleProductPage = ({rootType, rootId}) => {
 
     const {type, id} = useParams();
     return (
 
-        <>
-        <Link to={`/catalog/${type}`}> Back to catalog</Link>
-        <p>{products[type][id].name}</p>
-        <p>{products[type][id].descr}</p>
-        <p>{products[type][id].price}</p>
+        <div className="prod_page_container">
+            <div className="prod_back_link">
+                <Link to={`/catalog/${type}`}>← Back to catalog</Link>
+            </div>
+            <div className="prod_container">
+                <div className="prod_img">
+                    <img src={products[type][id].img} alt="" />
+                </div>
+                <div className="prod_info">
+                    <div className="header">
+                        <h3>{products[type][id].name}</h3>
+                        <p>{products[type][id].descr}</p>
+                    </div>
+                    <div className="price">
+                        <p>{`${products[type][id].price}  ₴`}</p>
+                        <CustomButton text="create order"/>
+                    </div>
+                </div>
+            </div>
 
-        </>
+
+        </div>
     )
 }
 
