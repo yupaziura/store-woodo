6e6437b4faa790221360b8f7f3fe4469a7d3843f
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-18 18:46:53 +0300

    Add style to product card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 
-const ProductCard = ({item, type, setRootId, num}) => {
+import './ProductCard.scss';
+
+const ProductCard = ({item, type, setRootId, num, img}) => {
     return (
-        <Link onClick={()=>setRootId(num)} to={`/catalog/${type}/${item.id}`}>
-            <div className="">
-                Name: {item.name}
-                Description: {item.descr}
-                Price: {item.price}
-            </div>
-        </Link>
+        
+            <Link className="card_link" onClick={()=>setRootId(num)} to={`/catalog/${type}/${item.id}`}>
+                <div className="card_custom">
+                    <img src={img} alt="" />
+                    <div className="card_data">
+                        <h4 className="card_title">{item.name}</h4>
+                        <p className="card_text card_price"><i>{`${item.price} ₴`}</i></p>
+                        <p className="card_text card_descr">{item.descr}</p>
+                    </div>
+                </div>
+
+            </Link>
     )
 }
 
