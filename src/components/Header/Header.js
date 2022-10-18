b39d608256196246a9ac47778d79980de44ee059
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-18 13:42:49 +0300

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {ReactComponent as ReactLogo} from '../../img/logo.svg';
 
 
 const Header = () => {
@@ -11,29 +12,30 @@ const Header = () => {
         <>
         <Navbar bg="light" variant="light">
             <Container>
-                <LinkContainer to="/"><Navbar.Brand>Main</Navbar.Brand></LinkContainer>
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <div style={{width: '120px'}}><ReactLogo/></div>
+                    </Navbar.Brand>
+                </LinkContainer>
                 
 
                 <Nav className="me-auto">
-                    <Nav.Link>
-                        <NavDropdown title="Catalog" id="collasible-nav-dropdown">
-                                <LinkContainer to="/catalog"><NavDropdown.Item>Catalog</NavDropdown.Item></LinkContainer>
-                                <NavDropdown.Divider />
-                                <LinkContainer to="/catalog/armchairs"><NavDropdown.Item >Armchairs</NavDropdown.Item></LinkContainer>
-                                <LinkContainer to="/catalog/tables"><NavDropdown.Item >Tables</NavDropdown.Item></LinkContainer>
-                                <LinkContainer to="/catalog/accessoires"><NavDropdown.Item>Accessoires</NavDropdown.Item></LinkContainer>
-                        </NavDropdown>
-                    </Nav.Link>
+                    
+                    <NavDropdown title="Catalog" id="collasible-nav-dropdown">
+                            <LinkContainer to="/catalog"><NavDropdown.Item>Catalog</NavDropdown.Item></LinkContainer>
+                            <NavDropdown.Divider />
+                            <LinkContainer to="/catalog/armchairs"><NavDropdown.Item >Armchairs</NavDropdown.Item></LinkContainer>
+                            <LinkContainer to="/catalog/tables"><NavDropdown.Item >Tables</NavDropdown.Item></LinkContainer>
+                            <LinkContainer to="/catalog/accessoires"><NavDropdown.Item>Accessoires</NavDropdown.Item></LinkContainer>
+                    </NavDropdown>
 
-
-
-                    <Nav.Link style={{alignSelf: 'center'}}>
-                        <LinkContainer to="/contacts" >
+                    <LinkContainer to="/contacts" >
+                        <Nav.Link style={{alignSelf: 'center'}}>
                             <Nav.Item >
                                 Contacts
                             </Nav.Item>
-                        </LinkContainer>
-                    </Nav.Link>
+                        </Nav.Link>
+                    </LinkContainer>
                 </Nav>
 
             </Container>
