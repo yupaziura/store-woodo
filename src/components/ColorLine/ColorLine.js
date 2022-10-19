68ce60d6fd08aebd3798a331a7e129fa7cf0c7d0
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-19 18:40:18 +0300

    Add active class to color line

--- src/components/ColorLine/ColorLine.js
+++ src/components/ColorLine/ColorLine.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import './ColorLine.scss';
 
 const ColorLine = ({array, setSmth, title}) => {
+
+    const [activeId, setActiveId] = useState('');
+
+
+    const selectAction = (item, i) => {
+        setSmth(item.type);
+        setActiveId(i);
+    }
+
 
     return (
         <div className="color_line">
             <h5>{title}</h5>
             {array.map((item, i)=> {
                 return (
-                    <div key={i} onClick={()=>{setSmth(item.type)}} className='color_pair'>
-                        <div className="color_button" style={{backgroundColor: item.color }}></div>
+                    <div key={i} onClick={()=>{selectAction(item, i)}} className={activeId === i? 'active color_pair' : 'color_pair'}>
+                        <div className={activeId === i? 'active color_button' : 'color_button'} style={{backgroundColor: item.color }}></div>
                         <div className="color_label">{item.name}</div>
                     </div>
                 )
