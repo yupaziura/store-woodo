cc6904d3de59682a5d0a525aa2ca897fc7762327
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-19 13:41:54 +0300

    Add styles to dialog window

--- src/components/DialogWindow/DialogWindow.js
+++ src/components/DialogWindow/DialogWindow.js
@@ -42,39 +42,51 @@ const DialogWindow = (props) => {
     return (
 
         <Modal fullscreen={true} show={props.show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Constructor</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form.Label>Select furniture type</Form.Label>
-                <Form.Select aria-label="Select" onChange={(e)=> {setConstrType(e.target.value)}}>
-                    <option selected disabled >Select an option</option>
-                    <option value="armchairs">armchair</option>
-                    <option value="tables">table</option>
-                </Form.Select>
 
 
 
-                {constrType ?
-                    <div className='constr_container' >
-                        <ColorLine title={'wood type'} setSmth={setConstrWood} array={arrWood}/>
-                        <div style={{width: '400px'}}>  
-                            {constrImg ? <img className='constr_img' src={constrImg} alt="" /> : null}
-                        </div>
-                        {
-                            constrType === 'armchairs'? <ColorLine title={'fabric type'} setSmth={setConstrFabric} array={arrFabric}/> : <div></div>
-                        }
-                    </div> 
-                    : 
-                    null
-                }
+            <Modal.Body >
+
+
+                <div style={{maxWidth: '1000px', margin: '0 auto'}}>
+
+                    <Modal.Header className="border border-0" closeButton>
+                        <h3 className="title">Constructor</h3>
+                    </Modal.Header>
+
+
+                    <Form.Label>Select furniture type</Form.Label>
+                    <Form.Select aria-label="Select" onChange={(e)=> {setConstrType(e.target.value)}}>
+                        <option selected disabled >Select an option</option>
+                        <option value="armchairs">armchair</option>
+                        <option value="tables">table</option>
+                    </Form.Select>
+
+
+
+                    {constrType ?
+                        <div className='constr_container' >
+                            <ColorLine title={'wood type'} setSmth={setConstrWood} array={arrWood}/>
+                            <div style={{width: '45%'}}>  
+                                {constrImg ? <img className='constr_img' src={constrImg} alt="" /> : null}
+                            </div>
+                            {
+                                constrType === 'armchairs'? <ColorLine title={'fabric type'} setSmth={setConstrFabric} array={arrFabric}/> : <div></div>
+                            }
+                        </div> 
+                        : 
+                        null
+                    }
+
+
+                    <div className='footer'>
+                        <Link to={`catalog/${constrType}/${constrId}`}>
+                            <Button  action={handleClose} text="View item"/>
+                        </Link>
+                    </div>
+
+                </div>
             </Modal.Body>
-
-            <Modal.Footer>
-                <Link to={`catalog/${constrType}/${constrId}`}>
-                    <Button  action={handleClose} text="View item"/>
-                </Link>
-            </Modal.Footer>
         </Modal>
     )
 }
